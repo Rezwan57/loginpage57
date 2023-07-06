a3ea389daf5276898ec157946172369bf7d1e2b1
Rezwan <rezwan57@outlook.com>
2023-07-06 15:06:50 +0600

    2nd

--- script.js
+++ script.js
@@ -76,15 +76,7 @@ const backToLogin = () => {
 }
 
 
-document.onreadystatechange =  () => {
-    if (document.readyState !== "complete") {
-        document.querySelector("body").style.visibility = "hidden";
-        document.querySelector(".spinner").style.display = "block";
-    } else {
-        document.querySelector(".spinner").style.display = "none";
-        document.querySelector("body").style.visibility = "visible";
-    }
-};
+
 
 
 const formAnimation = () => {
